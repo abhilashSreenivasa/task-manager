01619f584fe835e68da48c0cae4ae3c3386643d0
Abhilash Sreenivasa <abhilash.sreenivasa@gmail.com>
2025-11-16 12:21:38 +0530

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <div className="bg-white shadow-md rounded-xl p-6 w-full max-w-md">
+        <h1 className="text-2xl font-semibold text-gray-800 mb-4">
+          Tailwind is Working 🎉
+        </h1>
+
+        <p className="text-blue-600">
+          You can now start building your Dashboard and Task UI.
+        </p>
+
+        <button className="mt-6 bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
+          Test Button
+        </button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
